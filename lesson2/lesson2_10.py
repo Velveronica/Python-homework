a2a053756a828646f151383ed4ade08084b0cf07
Velveronica <velveronica@mail.ru>
2024-05-21 18:15:09 +0700

    first Dz2

---
+++ lesson2/lesson2_10.py
@@ -0,0 +1,7 @@
+var1=37
+var2=99
+var3=var1
+var4=var2
+var1=var4
+var2=var3
+print(var1, var2)
---
+++ lesson2/lesson2_4.py
@@ -0,0 +1,8 @@
+import math
+def square(x):
+      return x*x
+x=input("Введите число - ")
+x=float(x)
+x=math.ceil(x)
+s=square(x)
+print(s)
