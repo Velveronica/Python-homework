0566206743742083f665eaeac8c29213481ee755
Velveronica <velveronica@mail.ru>
2024-06-04 20:05:16 +0700

    task3

---
+++ lesson3/task_3/adress.py
@@ -0,0 +1,7 @@
+class adress:
+    def __init__(self, index, city, street, home, floor):
+        self.index=index
+        self.city=city
+        self.street=street
+        self.home=home
+        self.floor=floor
