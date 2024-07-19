1a15c9ea34c9485aa9418e9e1758f211a0a271a0
Velveronica <velveronica@mail.ru>
2024-07-19 21:25:44 +0700

    first trying

---
+++ lesson8/employeeApi.py
@@ -0,0 +1,62 @@
+
+import requests
+
+class EmployeeApi:
+
+    def __init__(self, url) -> None:
+        
+        self.url=url
+
+    def get_list_emp(self, params_to_add=None):
+
+        emp_list=requests.get(self.url+'/employee', params=params_to_add)
+        return emp_list.json()
+    
+    def get_token(self, user='bloom', password='fire-fairy'):
+        creds = {
+            "username": user,
+            "password": password
+        } 
+        resp = requests.post(self.url + '/auth/login', json=creds)
+        return resp.json()["userToken"]
+    
+    def create_emp(self, firstName, lastName, middleName, email, phone, id, isActive=True, url="", companyId="", birthdate=""):
+        
+        employee = {
+        "id": id,
+        "firstName": firstName,
+        "lastName": lastName,
+        "middleName": middleName,
+        "companyId": companyId,
+        "email": email,
+        "url": url,
+        "phone": phone,
+        "birthdate": birthdate,
+        "isActive": isActive
+}
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()
+        resp = requests.post(self.url + '/employee',json=employee, headers=my_headers)
+        return resp.json()
+    
+    def emp_id(self, id):
+
+        resp = requests.post(self.url + '/employee'+str(id))
+    
+    def edit(self, id, lastName, email, phone, isActive=True, url=""):
+
+        emp_editdata={
+        "lastName": lastName,
+        "email": email,
+        "url": url,
+        "phone": phone,
+        "isActive": isActive
+        }
+
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()
+        resp = requests.patch(self.url + '/employee'+str(id), headers=my_headers, json=emp_editdata)
+        return resp.json()
+
+
+
---
+++ lesson8/test_employee.py
@@ -0,0 +1,60 @@
+
+import requests
+from employeeApi import EmployeeApi
+
+api=EmployeeApi("https://x-clients-be.onrender.com")
+
+def test_get_emp():
+
+    body=api.get_list_emp()
+    assert len(body)>0
+
+def test_get_active_emp():
+
+    full_list=api.get_list_emp()
+    filtered_list = api.get_list_emp(params_to_add={"isActive": "true"})
+    assert len(full_list) >= len(filtered_list)
+
+
+def test_add_emp():
+
+    body=api.get_list_emp()
+    len_before=len(body)
+
+    id=666
+    firstName="Veronica" 
+    lastName="Procopets" 
+    middleName="Vadimovna" 
+    email="VV@mail.ru"
+    phone="86666666666"
+
+    result=api.create_emp(firstName, lastName, middleName, email, phone, id)
+    assert resp.status_code==201
+
+    body=api.get_list_emp()
+    len_after=len(body)
+    assert len_after-len_before==1
+    assert body[-1][firstName]==firstName
+    assert body[-1][lastName]==lastName
+    assert body[-1][id]==id
+
+def test_edit_emp():
+
+    id=666
+    firstName="Veronica" 
+    lastName="Procopets" 
+    middleName="Vadimovna" 
+    email="VV@mail.ru"
+    phone="86666666666"
+    isActive="isActive"
+
+    result=api.create_emp(firstName, lastName, middleName, email, phone, id)
+        
+    new_lastName="Popova"
+    new_phone="89996666666"
+    
+    edited=api.edit(new_lastName, new_phone)
+
+    assert edited[phone]==new_phone
+    assert edited[lastName]==new_lastName
+    assert edited[isActive]==True
