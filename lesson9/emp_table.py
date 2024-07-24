6be4bf2d25b131584ffc76c796e3821dfba6f9d2
Velveronica <velveronica@mail.ru>
2024-07-24 15:03:55 +0700

    hw9 work

--- lesson9/emp_table.py
+++ lesson9/emp_table.py
@@ -10,5 +10,22 @@ class EmployeeTable:
 
     def get_emp(self):
         
-        return self.db.execute("select * from employee").fetchall()
+        return self.db.execute("select * from employee where company_id=14732").fetchall()
+    
+    def delete(self, id):
+
+        self.db.execute(f"delete from employee where id ={id}")
+
+    def get_emp_id(self, id):
+
+        return self.db.execute(f"select * from employee where company_id=14732 and id={id}").fetchall()
+    
+    def get_maxid(self):
+
+        return self.db.execute(f"select max(id) from employee where company_id=14732").fetchall()[0][0]
+
+
+
+
+
         
--- lesson9/test_X_empdb.py
+++ lesson9/test_X_empdb.py
@@ -11,9 +11,50 @@ db=EmployeeTable("postgresql://x_clients_db_3fmx_user:mzoTw2Vp4Ox4NQH0XKN3KumdyA
 
 def test_get_emp():
 
-    api_result=api.get_list_emp()
+    api_result=api.get_list_emp(params_to_add={'company': 14732})
 
     db_result=db.get_emp()
 
     assert len(api_result)==len(db_result)
 
+def test_add_emp():
+
+
+    body=api.get_list_emp(params_to_add={'company': 14732})
+    len_before=len(body)
+
+
+    id=668
+    firstName="Veronica" 
+    lastName="Procopets" 
+    middleName="Vadimovna" 
+    companyId=14732
+    email="VV@mail.ru"
+    phone="86666666666"
+    birthdate= "2024-07-22T11:10:27.947Z"
+
+
+    result=api.create_emp(firstName, lastName, middleName, email, phone, id, companyId=companyId, birthdate=birthdate)
+    emp_id=result.json()["id"]
+ #   assert result.status_code==201
+
+    body=api.get_list_emp(params_to_add={'company': 14732})
+    len_after=len(body)
+
+ #   db.delete(id)
+    body_db=db.get_emp_id(emp_id)
+    
+    assert len_after-len_before==1
+    
+    assert body_db[-1][0]==emp_id
+
+def test_del():
+
+    len_before=len(api.get_list_emp(params_to_add={'company': 14732}))
+
+    maxid=db.get_maxid()
+    db.delete(maxid)
+
+    len_after=len(api.get_list_emp(params_to_add={'company': 14732}))
+
+    assert len_before-len_after==1
